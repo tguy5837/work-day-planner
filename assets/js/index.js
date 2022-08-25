6fdef7f8cc754b61a77bba9a10eb61ed166479ec
DTGuy <tguy5837@gmail.com>
2022-08-25 16:22:24 -0400

    trying to add time checker to change color of past items

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,27 @@
+// var
+
 $(currentDay).text(function () {
     var now = moment().format("dddd, MMM Do YYYY");
     return now;
-})
+});
 
 // var checkTime = function () {
 
 // }
 
-var timeCheck = $("time").attr("datetime");
-console.log(timeCheck);
+// var timeCheck = $("time").attr("datetime");
+// console.log(timeCheck);
+
+$(".slot time").each(function () {
+    var currentTimeSlot = $(this).attr("datetime");
+    console.log("Time Slot: " + currentTimeSlot);
+
+    var currentTime = moment().format("HH:mm");
+    console.log("Current Time: " + currentTime);
+
+    if (currentTime > currentTimeSlot) {
+        $(".slot input").addClass("list-group-item-dark");
+    } else if (currentTime < currentTimeSlot) {
+        $(".slot input").addClass("list-group-item-danger");
+    }
+})
