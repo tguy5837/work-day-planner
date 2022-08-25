5a3f018cc0261576251bd23dee6a1628bf61de77
DTGuy <tguy5837@gmail.com>
2022-08-25 15:18:39 -0400

    added time slots

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,10 @@ $(currentDay).text(function () {
     var now = moment().format("dddd, MMM Do YYYY");
     return now;
 })
+
+// var checkTime = function () {
+
+// }
+
+var timeCheck = $("time").attr("datetime");
+console.log(timeCheck);
